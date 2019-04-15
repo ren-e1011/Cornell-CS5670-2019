6c95f2c6e784928a66af13a5148265b961feae2d
marikacusick <mmc265@cornell.edu>
2019-04-15 17:13:12 -0400

    added the first todo

--- Project4_Stereo/student.py
+++ Project4_Stereo/student.py
@@ -32,7 +32,33 @@ def compute_photometric_stereo_impl(lights, images):
         normals -- float32 height x width x 3 image with dimensions matching
                    the input images.
     """
-    raise NotImplementedError()
+    height, width, channel = images[0].shape[0], images[0].shape[1],images[0].shape[2]
+    
+    albedo = np.zeros((height, width, channel), dtype = np.float32)
+    normal = np.zeros((height, width, 3), dtype = np.float32)
+    
+    num_images = len(images)
+    
+    for i in range(height):
+        #print (i)
+        for j in range(width):
+            for c in range(channel):
+                I = np.array([image[i,j,c] for image in images])
+                I.reshape((num_images,1))
+                L = lights.T
+                #print (L)
+                step1 = np.linalg.inv(np.dot(L, L.T))
+                G = np.dot(step1, np.dot(L, I))
+                p = np.linalg.norm(G)
+                if p < 1e-7:
+                    p, G_actual = 0, np.zeros((3,1))
+                else:
+                    G_actual = G/p
+                albedo[i,j,c] = p
+                normal[i,j] = G_actual.reshape((3,))
+
+    return albedo, normal
+
 
 
 
